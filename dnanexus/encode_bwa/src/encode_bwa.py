4ccdfeeb402e31408a82b087dc0860c46c2de366
J. Seth Strattan <jseth@stanford.edu>
2014-08-07 23:37:32 -0700

    Support paired end input

--- dnanexus/encode_bwa/src/encode_bwa.py
+++ dnanexus/encode_bwa/src/encode_bwa.py
@@ -20,6 +20,28 @@ import subprocess, shlex
 from multiprocessing import Pool, cpu_count
 import pdb
 
+def run_pipe(steps, outfile=None):
+    #break this out into a recursive function
+    from subprocess import Popen, PIPE
+    p = None
+    p_next = None
+    first_step_n = 1
+    last_step_n = len(steps)
+    for n,step in enumerate(steps, start=first_step_n):
+        if n == first_step_n:
+            p = Popen(shlex.split(step), stdout=PIPE)
+        elif n == last_step_n and outfile: #only treat the last step specially if you're sending stdout to a file
+            with open(outfile, 'w') as fh:
+                p_last = Popen(shlex.split(step), stdin=p.stdout, stdout=fh)
+                p.stdout.close()
+                p = p_last
+        else: #handles intermediate steps and, in the case of a pipe to stdout, the last step
+            p_next = Popen(shlex.split(step), stdin=p.stdout, stdout=PIPE)
+            p.stdout.close()
+            p = p_next
+    out,err = p.communicate()
+    return out,err
+
 @dxpy.entry_point("postprocess")
 def postprocess(indexed_reads, unmapped_reads, reference_tar):
 
@@ -50,47 +72,59 @@ def postprocess(indexed_reads, unmapped_reads, reference_tar):
     # assume the reference file is the only .fa file
     reference_filename = subprocess.check_output('ls *.fa', shell=True).rstrip()
 
-    reads_basename = unmapped_reads_filenames[0].rstrip('.gz').rstrip('.fq').rstrip('.fastq')
+    if paired_end:
+        r1_basename = unmapped_reads_filenames[0].rstrip('.gz').rstrip('.fq').rstrip('.fastq')
+        r2_basename = unmapped_reads_filenames[1].rstrip('.gz').rstrip('.fq').rstrip('.fastq')
+        reads_basename = r1_basename + r2_basename
+    else:
+        reads_basename = unmapped_reads_filenames[0].rstrip('.gz').rstrip('.fq').rstrip('.fastq')
     raw_bam_filename = '%s.raw.srt.bam' %(reads_basename)
     raw_bam_mapstats_filename = '%s.raw.srt.bam.flagstat.qc' %(reads_basename)
 
     paired_end = len(indexed_reads) == 2
 
     if paired_end:
-        with dxpy.new_dxfile('w', name=raw_bam_filename) as mapped_reads:
-            mapped_reads.write("Mapping: Paired end not yet implemented")
-        with dxpy.new_dxfile('w', name=raw_nam_mapstats_filename) as mapping_statistics:
-            mapping_statistics.write("Mapping statistics: Paired end not yet implemented")
+        reads1_filename = indexed_reads_filenames[0]
+        reads2_filename = indexed_reads_filenames[1]
+        unmapped_reads1_filename = unmapped_reads_filenames[0]
+        unmapped_reads2_filename = unmapped_reads_filenames[1]
+        raw_sam_filename = reads_basename + ".raw.sam"
+        raw_bam_prefix = reads_basename + ".raw.srt"
+        badcigar_filename = "badreads.tmp"
+        steps = [ "bwa sampe %s %s %s %s %s" %(reference_filename, reads1_filename, reads2_filename, unmapped_reads1_filename, unmapped_reads2_filename),
+                  "tee %s" %(raw_sam_filename),
+                  r"""awk 'BEGIN {FS="\t" ; OFS="\t"} ! /^@/ && $6!="*" { cigar=$6; gsub("[0-9]+D","",cigar); n = split(cigar,vals,"[A-Z]"); s = 0; for (i=1;i<=n;i++) s=s+vals[i]; seqlen=length($10) ; if (s!=seqlen) print $1 ; }'""",
+                  "sort",
+                  "uniq" ]
+        out,err = run_pipe(steps,badcigar_filename)
+        if err:
+            print "sampe error: %s" %(err)
+        steps = [ "cat %s" %(raw_sam_filename),
+                  "grep -v -F -f %s" %(badcigar_filename),
+                  "samtools view -Su -",
+                  "samtools sort - %s" %(raw_bam_filename).rstrip('.bam') ] # samtools adds .bam
+        out,err = run_pipe(steps)
     else:
-        steps = [ "bwa samse %s %s %s" %(reference_filename, indexed_reads_filenames[0], unmapped_reads_filenames[0]),
+        reads_filename = indexed_reads_filenames[0]
+        unmaped_reads_filename = unmapped_reads_filenames[0]
+        steps = [ "bwa samse %s %s %s" %(reference_filename, reads_filename, unmaped_reads_filename),
                   "samtools view -Su -",
                   "samtools sort - %s" %(raw_bam_filename).rstrip('.bam') ] # samtools adds .bam
 
-        #break this out into a recursive function
-        from subprocess import Popen, PIPE
-        p = None
-        p_next = None
-        for n,step in enumerate(steps, start=1):
-            if n == 1:
-                p = Popen(shlex.split(step), stdout=PIPE)
-            else:
-                p_next = Popen(shlex.split(step), stdin=p.stdout, stdout=PIPE)
-                p.stdout.close()
-                p = p_next
-        out,err = p.communicate()
+        out,err = run_pipe(steps)
 
-        if out:
-            print "sam output: %s" %(out)
-        if err:
-            print "sam error: %s" %(err)
+    if out:
+        print "samtools output: %s" %(out)
+    if err:
+        print "samtools error: %s" %(err)
 
-        with open(raw_bam_mapstats_filename, 'w') as fh:
-            subprocess.check_call(shlex.split("samtools flagstat %s" \
-                %(raw_bam_filename)), stdout=fh)
+    with open(raw_bam_mapstats_filename, 'w') as fh:
+        subprocess.check_call(shlex.split("samtools flagstat %s" \
+            %(raw_bam_filename)), stdout=fh)
 
-        print subprocess.check_output('ls', shell=True)
-        mapped_reads = dxpy.upload_local_file(raw_bam_filename)
-        mapping_statistics = dxpy.upload_local_file(raw_bam_mapstats_filename)
+    print subprocess.check_output('ls', shell=True)
+    mapped_reads = dxpy.upload_local_file(raw_bam_filename)
+    mapping_statistics = dxpy.upload_local_file(raw_bam_mapstats_filename)
 
     output = { "mapped_reads": dxpy.dxlink(mapped_reads),
                "mapping_statistics": dxpy.dxlink(mapping_statistics) }
