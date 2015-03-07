da049e278abf710a0a09558c6be5ebbd9af9168e
J. Seth Strattan <jseth@stanford.edu>
2015-03-06 19:54:20 -0800

    Add bedtools to resources; add dependencies

--- dnanexus/bam2tagAlign/src/bam2tagAlign.py
+++ dnanexus/bam2tagAlign/src/bam2tagAlign.py
@@ -11,8 +11,42 @@
 # DNAnexus Python Bindings (dxpy) documentation:
 #   http://autodoc.dnanexus.com/bindings/python/current/
 
-import os
+import os, subprocess, shlex
 import dxpy
+
+def run_pipe(steps, outfile=None):
+    #break this out into a recursive function
+    #TODO:  capture stderr
+    from subprocess import Popen, PIPE
+    p = None
+    p_next = None
+    first_step_n = 1
+    last_step_n = len(steps)
+    for n,step in enumerate(steps, start=first_step_n):
+        print "step %d: %s" %(n,step)
+        if n == first_step_n:
+            if n == last_step_n and outfile: #one-step pipeline with outfile
+                with open(outfile, 'w') as fh:
+                    print "one step shlex: %s to file: %s" %(shlex.split(step), outfile)
+                    p = Popen(shlex.split(step), stdout=fh)
+                break
+            print "first step shlex to stdout: %s" %(shlex.split(step))
+            p = Popen(shlex.split(step), stdout=PIPE)
+            #need to close p.stdout here?
+        elif n == last_step_n and outfile: #only treat the last step specially if you're sending stdout to a file
+            with open(outfile, 'w') as fh:
+                print "last step shlex: %s to file: %s" %(shlex.split(step), outfile)
+                p_last = Popen(shlex.split(step), stdin=p.stdout, stdout=fh)
+                p.stdout.close()
+                p = p_last
+        else: #handles intermediate steps and, in the case of a pipe to stdout, the last step
+            print "intermediate step %d shlex to stdout: %s" %(n,shlex.split(step))
+            p_next = Popen(shlex.split(step), stdin=p.stdout, stdout=PIPE)
+            p.stdout.close()
+            p = p_next
+    out,err = p.communicate()
+    return out,err
+
 
 @dxpy.entry_point('main')
 def main(input_bam, paired_end):
@@ -33,10 +67,11 @@ def main(input_bam, paired_end):
         end_infix = 'SE'
     final_TA_filename = input_bam_basename + '.' + end_infix + '.tagAlign.gz'
 
+    print subprocess.check_output('ls -l', shell=True)
+
     # ===================
     # Create tagAlign file
     # ===================
-
     out,err = run_pipe([
         "bamToBed -i %s" %(input_bam_filename),
         r"""awk 'BEGIN{OFS="\t"}{$4="N";$5="1000";print $0}'""",
@@ -58,6 +93,7 @@ def main(input_bam, paired_end):
             "bamToBed -bedpe -mate1 -i %s" %(final_nmsrt_bam_filename),
             "gzip -c"],
             outfile=final_BEDPE_filename)
+
     print subprocess.check_output('ls -l', shell=True)
 
 
