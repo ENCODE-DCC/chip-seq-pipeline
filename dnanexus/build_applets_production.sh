bd824be8b128fbd96d1acd33de0ee1abb46f1c3e
J. Seth Strattan <jseth@stanford.edu>
2016-02-03 11:11:13 -0800

    Can now specify destination project and folder

--- dnanexus/build_applets_production.sh
+++ dnanexus/build_applets_production.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 DEFAULT_PROJECT='ENCODE - ChIP Production'
+DEFAULT_FOLDER='applets/'
 
 project=${1:-$DEFAULT_PROJECT}
+folder=${2:-$DEFAULT_FOLDER}
+
 APPLETS=('input_shield' 'accession_analysis' 'encode_bwa' 'filter_qc' 'xcor' 'xcor_only' 'spp' 'pool' 'pseudoreplicator' 'encode_spp' 'encode_macs2' 'macs2' 'idr' 'idr2' 'encode_idr' 'overlap_peaks')
 
 for appl in ${APPLETS[@]}; do
-	dest="$project:/applets/$appl"
+		dest="$project:$folder$appl"
 	echo $dest
 	dx build --archive --destination "$dest" "$appl/"
 done
