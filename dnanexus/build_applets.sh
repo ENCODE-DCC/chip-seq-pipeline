b37d729efc6d7326faea686a0e1dfa449f0e3025
J. Seth Strattan <jseth@stanford.edu>
2017-03-27 14:45:53 -0700

    Build all applets; remove project default

--- dnanexus/build_applets.sh
+++ dnanexus/build_applets.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-DEFAULT_PROJECT='ENCODE - ChIP Production'
 DEFAULT_FOLDER='/applets/'
 
-project=${1:-$DEFAULT_PROJECT}
+project=$1
 folder=${2:-$DEFAULT_FOLDER}
 
 PRODUCTION_APPLETS=('encode_map' 'filter_qc' 'xcor' 'xcor_only' 'spp' 'pool' 'pseudoreplicator' 'encode_spp' 'encode_macs2' 'macs2' 'idr2' 'encode_idr' 'overlap_peaks')
 ACCESSORY_APPLETS=('input_shield' 'accession_analysis' 'shell')
 
-# for appl in ${PRODUCTION_APPLETS[@]}; do
-# 		dest="$project:$folder$appl"
-# 	echo $dest
-# 	cp common.py $appl/resources/home/dnanexus/common.py
-# 	dx build --archive --destination "$dest" "$appl/"
-# done
+for appl in ${PRODUCTION_APPLETS[@]}; do
+		dest="$project:$folder$appl"
+	echo $dest
+	cp common.py $appl/resources/home/dnanexus/common.py
+	dx build --archive --destination "$dest" "$appl/"
+done
 
 for appl in ${ACCESSORY_APPLETS[@]}; do
 		dest="$project:$folder$appl"
